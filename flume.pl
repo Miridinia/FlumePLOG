3ff294c04893edfed50f5bd0d8c12a7c21488431
Unknown <up201603811@fe.up.pt>
2018-11-18 15:51:00 +0000

    RedNum, BlueNum and Blank (game_over doesnt work yet)

--- flume.pl
+++ flume.pl
@@ -71,21 +71,47 @@ print_piece(Piece) :-
 print_separator :- 
 		print('*-*-*-*-*-*-*-*\n').
 					
-%GameState = [Board, Player]
-gamestate(Board, Player, [Board, Player]).
-get_board([Board,_], Board).
-get_player([_, Player], Player).
+%GameState = [Board, Player, Red_Num, Blue_num]
+gamestate(Board, Player, Red_Num, Blue_Num, [Board, Player, Red_Num, Blue_Num]).
+get_board(GameState, Board):-
+	nth1(1, GameState, Board).
+get_player(GameState, Player):-
+	nth1(2, GameState, Player).
+get_red_num(GameState, Red_Num):-
+	nth1(3, GameState, Red_Num).
+get_blue_num(GameState, Blue_Num):-
+	nth1(4, GameState, Blue_Num).
 
-set_board([_, Player], NewBoard, [NewBoard, Player]).
-set_player([Board, _], NewPlayer, [Board, NewPlayer]).
 
-test :- 
-	initial_game_state(GameState),
-	make_move(GameState, [1, 5], NewGameState),
-	get_board(NewGameState, Board),
-	get_player(NewGameState, Player),
-	print_board(Board),
-	print(Player).
+set_gamestate_elem(1, Elem, [_|GameState], [Elem|GameState]).
+set_gamestate_elem(Index, Elem, [H|GameState], [H|NewGameState]):-
+	Index > 1,
+	Index_1 is Index-1,
+	set_gamestate_elem(Index_1, Elem, GameState, NewGameState).
+
+set_board(GameState, NewBoard, NewGameState):-
+	set_gamestate_elem(1, NewBoard, GameState, NewGameState).
+set_player(GameState, NewPlayer, NewGameState):-
+	set_gamestate_elem(2, NewPlayer, GameState, NewGameState).
+set_red_num(GameState, NewRedNum, NewGameState):-
+	set_gamestate_elem(3, NewRedNum, GameState, NewGameState).
+set_blue_num(GameState, NewBlueNum, NewGameState):-
+	set_gamestate_elem(4, NewBlueNum, GameState, NewGameState).
+
+increment_Red(GameState, NewGameState):-
+	get_red_num(GameState, RedNum),
+	NewRedNum is RedNum + 1,
+	set_red_num(GameState, NewRedNum, NewGameState).
+increment_Blue(GameState, NewGameState):-
+	get_blue_num(GameState, BlueNum),
+	NewBlueNum is BlueNum + 1,
+	set_blue_num(GameState, NewBlueNum, NewGameState).
+
+increment(GameState, red, NewGameState):-
+	increment_Red(GameState, NewGameState).
+increment(GameState, blue, NewGameState):-
+	increment_Blue(GameState, NewGameState).
+
 
 start :- 
 	initial_game_state(GameState),
@@ -94,22 +120,28 @@ start :-
 game_loop(GameState):-
 	get_board(GameState, Board),
 	print_board(Board),
-	\+ game_finished(GameState),
+	\+ game_over(GameState, Winner),
 	play(GameState, NewGameState),
 	game_loop(NewGameState).
 
 initial_game_state(GameState):-
 	initial_board(Board),
-	gamestate(Board, red, GameState).
+	gamestate(Board, red, 0, 0, GameState).
 		
 play(GameState, NextGameState):-
 	get_move(Pos),
 	valid_move(GameState, Pos), 
 	make_move(GameState, Pos, NextGameState).
 
-game_finished(GameState):-
-	get_board(GameState, Board),
-	\+(get_piece(Board, [X, Y], Elem), Elem ==blank).
+
+game_over([_Board, _Player, Red, Blue], Winner) :-
+	Red + Blue =:= 25,
+	get_winner(Red, Blue, Winner).
+	
+get_winner(Red, Blue, 'red') :-
+	Red > Blue, !.
+
+get_winner(_Red, _Blue, 'blue').
 
 
 valid_move(GameState, Pos):-
@@ -133,16 +165,6 @@ set_piece_in_row([_|NextCells], 0, NewPiece, [NewPiece|NextCells]).
 set_piece_in_row([Cell|NextCells], ColumnNumber, NewPiece, [Cell|NewNextCells]):-
 	NewColumnNumber is ColumnNumber - 1,
 	set_piece_in_row(NextCells, NewColumnNumber, NewPiece, NewNextCells). 
-
-next_player(blue, red).
-next_player(red, blue).
-
-same_player(GameState, Pos) :-
-	get_board(GameState, Board),
-	get_adjacent_pieces(Board, Pos, Pieces),
-	delete(Pieces, blank, NonBlank),
-	length(NonBlank, Length),
-	Length > 2.	
 
 get_adjacent_pieces(Board, Pos, [Up, Down, Left, Right]) :-
 	get_piece_up(Board, Pos, Up),
@@ -171,8 +193,22 @@ make_move(GameState, Pos, NextGameState) :-
 	get_player(GameState, Player),
 	set_piece(Board, Pos, Player, NewBoard),
 	set_board(GameState, NewBoard, NewGameState),
+	increment(NewGameState, Player, NewGameState_temp),
+	value(NewGameState_temp, Blank),
+
 	decide_next_player(NewGameState, Pos, NewPlayer),
-	set_player(NewGameState, NewPlayer, NextGameState).
+	set_player(NewGameState_temp, NewPlayer, NextGameState).
+
+
+next_player(blue, red).
+next_player(red, blue).
+
+same_player(GameState, Pos) :-
+	get_board(GameState, Board),
+	get_adjacent_pieces(Board, Pos, Pieces),
+	delete(Pieces, blank, NonBlank),
+	length(NonBlank, Length),
+	Length > 2.	
 
 decide_next_player(GameState, Pos, Player):-
 	same_player(GameState, Pos),
@@ -182,6 +218,8 @@ decide_next_player(GameState, Pos, NewPlayer):-
 	\+ same_player(GameState, Pos),
 	get_player(GameState, Player),
 	next_player(Player, NewPlayer).
+
+
 
 get_move(Pos):-
 	repeat,
@@ -198,3 +236,13 @@ check_number(Num):-
 	char_code(Char, Num),
 	Char @> '0',
 	Char @< '6'.
+
+value(GameState, Blank):-
+	get_red_num(GameState, Red_Num),
+	get_blue_num(GameState, Blue_Num),
+	Total is Red_Num + Blue_Num,
+	Blank is 25 - Total,
+
+	format('Red: ~w ', Red_Num),
+	format('Blue: ~w ', Blue_Num),
+	format('Blanks: ~w', Blank), nl.
